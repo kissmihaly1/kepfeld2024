f1c43f6dc8ddcbf50b12c736d50d34775dac71c4
weezard <veelmoth@gmail.com>
2024-11-06 22:07:29 +0100

    update endpoint frissítése
    - Frissítettem az endpoint-ot. A frotnend oldaláról meghívtam ezen endpointot egy arraybuffer-t tartalmazó api request-el azon célból, hogy ezen arraybuffer-ből készítsek egy videó fájlt, és azt eltároljam a könyvtárban "media-file.mp4" néven. Ezen fájlra meghívhatnánk a továbbiakban a yolo algoritmust.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
@@ -8,9 +10,27 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/upload')
-def upload():
-    pass
+@app.route('/upload', methods=["POST"])
+def upload_media():
+    """
+    Vár egy Arraybuffer api request-et, majd létrehoz számára egy fájlt
+    a következő elérési útvonalon: media-file.mp4
+    :return: Szöveg arról, hogy sikerült a fájlfeltöltés
+    """
+    media_file = open("media-file.mp4", "wb")
+    media_file.write(request.data)
+    media_file.close()
+
+    # Feltöltésre került az mp4 fájl, és az el van tárolva a következő helyen: media-file.mp4
+    # TODO: Hívd meg ezen videóra a YOLO algoritmust, és hozz létre egy media-file-result.mp4 videót az eredménynek (amennyiben lehetséges)
+    return "Finished"
+
+
+@app.route('/fetch', methods=["GET"])
+def fetch_media():
+    # TODO: A yolo algoritmus eredményét olvasd be (ami feltehetően egy mp4) videó és küld vissza azt a frontend-nek.
+    return "Finished"
+
 
 if __name__ == '__main__':
     app.run()
