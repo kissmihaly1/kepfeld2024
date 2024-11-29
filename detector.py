cf40b70b8f7550aaee0bb6d936aa9485d8ab9193
Zsurzsa Dóra <h054067@stud.u-szeged.hu>
2024-11-29 08:44:09 +0100

    detektalas es szamlalas pontositasa

--- detector.py
+++ detector.py
@@ -1,6 +1,5 @@
 import cv2
 import base64
-from collections import defaultdict
 from ultralytics import YOLO
 from datetime import datetime
 import numpy as np
@@ -13,6 +12,7 @@ class VehicleDetectionTracker:
                  position_threshold=50,
                  time_threshold=3,
                  stationary_threshold=5,
+                 speed_threshold=2,  # sebesség küszöb (pixelek/másodperc)
                  font_scale=1.2,
                  shadow_color=(0, 0, 0),
                  thickness=3,
@@ -20,7 +20,6 @@ class VehicleDetectionTracker:
                  font=cv2.FONT_HERSHEY_SIMPLEX,
                  position=(10, 30)):
         self.model = YOLO(model_path)  # modell betoltese
-        self.track_history = defaultdict(list)  # autok utvonala
         self.detected_vehicles = set()  # mar eszlelt jarmuvek azonositoja
         self.car_count = 0  # osszes eszlelt auto szama
         self.last_detection_time = {}  # utolso eszleles idopontja
@@ -29,6 +28,7 @@ class VehicleDetectionTracker:
         self.position_threshold = position_threshold  # min. poz. valtoztatas (pixelben)
         self.time_threshold = time_threshold  # min. idoelteres uj eszleleshez (mp-ben)
         self.stationary_threshold = stationary_threshold  # min. ido, amig az auto allhat
+        self.speed_threshold = speed_threshold  # sebesség küszöb
         self.font_scale = font_scale
         self.shadow_color = shadow_color
         self.thickness = thickness
@@ -55,6 +55,12 @@ class VehicleDetectionTracker:
     # 2 poz. tavolsaganak kiszam.
     def calculate_distance(self, pos1, pos2):
         return np.sqrt((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2)
+
+    # sebesseg kiszam. (pixel/mp)
+    def calculate_speed(self, car_center, last_position, time_interval):
+        distance = self.calculate_distance(car_center, last_position)
+        speed = distance / time_interval  # sebesség: pixelek per másodperc
+        return speed
 
     # adott kepkocka feldolgozasa es auto felism.
     def process_frame(self, frame):
@@ -86,11 +92,16 @@ class VehicleDetectionTracker:
                             car_center, self.previous_positions.get(track_id, car_center)
                         )
 
-                        # idolimit lejart + kocsi mozgott vagy tul sokaig nem mozgott
+                        # auto mozgott vagy hosszu ideig nem volt eszlelve
                         if time_since_last_detection > self.time_threshold or distance_moved > self.position_threshold:
-                            if track_id not in self.detected_vehicles:  # uj kocsi, szamlalo noveles
-                                self.detected_vehicles.add(track_id)
-                                self.car_count += 1
+                            speed = self.calculate_speed(car_center, self.previous_positions.get(track_id, car_center),
+                                                         time_since_last_detection)
+
+                            # sebesseg > kuszob, auto detektalasa
+                            if speed > self.speed_threshold:
+                                if track_id not in self.detected_vehicles:  # uj kocsi, szamlalo noveles
+                                    self.detected_vehicles.add(track_id)
+                                    self.car_count += 1
 
                             # ido es poz. frissites
                             self.last_detection_time[track_id] = current_time
@@ -98,23 +109,17 @@ class VehicleDetectionTracker:
 
                         # tul hosszu ideig all detektalt auto, de meg nincs regisztralva
                         elif time_since_last_detection > self.stationary_threshold:
-                            # csak akkor det. ujra ha az auto mozog
                             if track_id in self.detected_vehicles:
                                 self.last_detection_time[track_id] = current_time
 
-                        # ID és keret megjelenitese
+                        # ID és keret megjel.enitese
                         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
-                        cv2.putText(frame, f'ID: {track_id}', (x1, y1 - 10), self.font, 0.6, (0, 255, 0),
-                                    2)
+                        cv2.putText(frame, f'ID: {track_id}', (x1, y1 - 10), self.font, 0.6, (0, 255, 0), 2)
 
             response["number_of_vehicles_detected"] = len(self.detected_vehicles)
 
         # szamlalo megjel. arnyekkal
-        self.draw_text_with_shadow(
-            frame,
-            f'Total Vehicles Counted: {self.car_count}'
-        )
+        self.draw_text_with_shadow(frame, f'Total Vehicles Counted: {self.car_count}')
 
         # kepkocka base64 formatumba kodolasa
         original_frame_base64 = self._encode_image_base64(frame)
@@ -131,7 +136,7 @@ class VehicleDetectionTracker:
             if not success:
                 break
 
-            # Képkocka autó felismeréshez
+            # kepkocka auto felism.
             self.process_frame(frame)
 
             # kepkocka atmeretezese
@@ -157,6 +162,7 @@ class VehicleDetectionTracker:
 
 
 if __name__ == '__main__':
-    video_path = 'videok/1_iranyban_1.mp4'
-    tracker = VehicleDetectionTracker(position_threshold=50, time_threshold=10, stationary_threshold=10)
+    video_path = 'videok/1_iranyban_1-converted.mp4'
+    tracker = VehicleDetectionTracker(position_threshold=50, time_threshold=10, stationary_threshold=10,
+                                      speed_threshold=2)
     tracker.process_video(video_path)
